6563637b7230588ef85cdd6ad7609d492fe991fe
Matheus Silva <matheus.moraes09@hotmail.com>
2025-07-25 16:21:28 -0300

    variaveis de ambiente para a chave da api

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -7,10 +7,8 @@ export default axios.create({
 export function api_options() {
   return {
     params:{
-      // api_key:process.env.REACT_APP_KEY ,
-      // language:process.env.REACT_APP_LANG
-        api_key:'33e0e563cce9786eea14d6fc198c9afe',
-        language:'pt-BR'
+      api_key:process.env.REACT_APP_KEY ,
+      language:process.env.REACT_APP_LANG
   }
   };
 }
